a34f86e84fba396bda4a873795fa2c3bb6724527
alexandret68 <26066551+alexandret68@users.noreply.github.com>
2019-01-03 10:04:48 +0100

    Update temperature.go

--- temperature.go
+++ temperature.go
@@ -2,10 +2,10 @@ package temperature
 
 type Temperature float64
 
-func CtoF(c float64) float64 {
-	return(c*(9/5))+32
+func CtoF(c float64) Temperature {
+	return Temperature((c*(9/5))+32)
 }
 
-func FtoC(f float64) float64 {
-	return(f-32)*(9/5)
+func FtoC(f float64) Temperature {
+	return Temperature((f-32)*(9/5))
 }
